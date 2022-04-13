8ffba266c14a6f7a86077e6ff7adffb86f77c311
Chloe Ho <98048503+chloeho12@users.noreply.github.com>
2022-04-13 19:26:30 +0000

    A3 - .readline() method return

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 # lab09
 # chloe and heidi
 
-datafile = open("training.txt")
-trainStr = datafile.readline()
-print(trainStr)
+train = open('validation.txt')
+count = 0
+line = train.readline()
+while line:
+	count = count + 1
+	line = train.readline()
+train.close()
+print('validation.txt has ' + str(count) + ' lines.') 
 
-datafile = datafile.close()
-#When we run the program multiple times, the program still prints the first line.
+# A. How many lines are in the training data set?
+	# there are 8427 lines in the training data set.
+
+# B. How many lines are in the validation data set?
+	# there are 101 lines in the training data set.
+
+# C. Based on the behavior of your program, what value does the .readline() method return when all of the lines in the file have been read? That is, what is the value of line that causes the while loop to terminate?
+	# It returns a value of False since it has no data to be passed through it, which allows the while loop to be escaped
