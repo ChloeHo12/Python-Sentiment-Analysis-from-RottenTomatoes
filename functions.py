054cee0d49126ae6fc1236112c23e8cb57ac0100
Heidi Beardsley <101588748+HeidiBeardsley@users.noreply.github.com>
2022-04-13 19:34:17 +0000

    B1a - getSentRate(review)

--- functions.py
+++ functions.py
@@ -1,1 +1,5 @@
+# chloe and heidi
 # Your functions go here.
+def getSentRate(review):
+	sentRate = int(review[0])
+	return sentRate
--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 # lab09
 # chloe and heidi
+from functions import *
 
 train = open('validation.txt')
-count = 0
 line = train.readline()
-while line:
-	count = count + 1
-	line = train.readline()
-train.close()
-print('validation.txt has ' + str(count) + ' lines.') 
 
-# A. How many lines are in the training data set?
-	# there are 8427 lines in the training data set.
+print(getSentRate(line))
 
-# B. How many lines are in the validation data set?
-	# there are 101 lines in the training data set.
-
-# C. Based on the behavior of your program, what value does the .readline() method return when all of the lines in the file have been read? That is, what is the value of line that causes the while loop to terminate?
-	# It returns a value of False since it has no data to be passed through it, which allows the while loop to be escaped
+assert getSentRate('4 I laughed, I cried, it was better than cats.') == 4
+'sentiment should be 4'
+assert getSentRate('0 Two thumbs down.') == 0, 'sentiment should be 0'
+print("Success!") 
