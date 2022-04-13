0b0c3eaac98bd4c9620b5c8c9a332262f7580138
Chloe Ho <98048503+chloeho12@users.noreply.github.com>
2022-04-13 19:18:05 +0000

    A1 - open the file and read the first line

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # lab09
 # chloe and heidi
+
+inFile = open("training.txt")
+trainStr = inFile.readline()
+print(trainStr)
+
+#When we run the program multiple times, the program still prints the first line.
