ee392449ecfc0f42646ba8ef8005ced603bf3b7c
Chloe Ho <98048503+chloeho12@users.noreply.github.com>
2022-04-15 03:54:38 +0000

    B2a - wordSent()

--- functions.py
+++ functions.py
@@ -1,17 +1,48 @@
 # chloe and heidi
 # Your functions go here.
+
 def getSentRate(reviewSR):
+	"""takes a string containing both a sentiment rating and the associated review text"""
 	sentRate = int(reviewSR[0])
 	return sentRate
 
 def getRevText(reviewRT):
+	"""takes a string containing a sentiment rating and the associated review text"""
 	reviewStr = (reviewRT[2:])
 	return reviewStr
 
 def cleanText(reviewCT):
+	"""takes in a String containing some text and
+returns a cleaned up version of the string"""
 	rev = reviewCT.lower().strip()
 	for char in rev:
 		if not char.isalpha() and not char == " ":
 			rev = rev.replace(char, "")
 	rev = rev.replace("  ", " ")
 	return rev
+
+def wordSent(word):
+	"""returns the sentiment rating of the word based on all of the reviews in the training.txt file"""
+	# search for the word in getRevText
+	train = open('training.txt')
+	line = train.readline()
+	wordCount = 0
+	sentTot = 0
+	while line:
+		rev = getRevText(line)
+		cleanLine = cleanText(rev)
+		#check if the word is in the clean line
+		if word.lower() in cleanLine:
+			wordCount += 1
+			sentRate = getSentRate(line)
+			sentTot += sentRate
+		line = train.readline()
+	wordAvg = sentTot/wordCount
+	return wordAvg
+
+
+
+
+
+		
+	
--- main.py
+++ main.py
@@ -13,3 +13,14 @@ while line:
 	line = train.readline() 
 avg = sentTot/count
 print("Average sentiment rating:", avg)
+
+train.close()
+
+epsilon = 0.0001
+
+assert abs(wordSent('Fabulous') - 2.75) < epsilon, 'fabulous: wrong SR'
+assert abs(wordSent('boring') - 1.1428571428571428) < epsilon,'boring: wrong SR'
+assert abs(wordSent('love') - 2.574660633484163) < epsilon, 'love: wrong SR'
+assert abs(wordSent('hate') - 1.7954545454545454)< epsilon, 'hate: wrong SR'
+assert abs(wordSent('the') - 2.038604742308446) < epsilon, 'the: wrong SR'
+print('Success!')
