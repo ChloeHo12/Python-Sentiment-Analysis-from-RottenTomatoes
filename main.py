7e902069efcc9b21d90d1f59ceec954495aa2904
Chloe Ho <98048503+chloeho12@users.noreply.github.com>
2022-04-18 19:33:21 +0000

    B3a - read the first review and split

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 # lab09
 # chloe and heidi
+
 from functions import *
 
-userText = input('Enter the text that you want to know the sentiment rating >> ')
-userSent = textSent(userText)
-if userSent >= 2.31:
-	print("The sentiment rating is", userSent,". This is highly positive :):)")
-elif 2.19 <= userSent < 2.31:
-	print("The sentiment rating is", userSent,". This is positive :)")
-elif 1.8 <= userSent < 2.19:
-	print("The sentiment rating is", userSent,". This is neutral. :|")
-elif 1.6 <= userSent < 1.8:
-	print("The sentiment rating is", userSent,". This is negative :(.")
-else:
-	print("The sentiment rating is", userSent, ". This is highly negative :(:(")
+valid = open('validation.txt')
+line = valid.readline()
+print(getSentRate(line))
+print(getRevText(line))
 
