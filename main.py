87e1bf8f06ef33328356260682af8dbc6835e24d
Chloe Ho <98048503+chloeho12@users.noreply.github.com>
2022-04-18 19:34:55 +0000

    B3c - computes and displays errors

--- main.py
+++ main.py
@@ -3,13 +3,21 @@
 
 from functions import *
 
+#a
 valid = open('validation.txt')
 line = valid.readline()
 print(getSentRate(line))
 print(getRevText(line))
 
+#b
 rev = getRevText(line)
 #Computer's rating
 print(textSent(rev))
 #Actual human rating: 1 (negative)
 print(getSentRate(line))
+
+#c
+lineSentRate = textSent(line)
+lineHumanRate = getSentRate(line)
+sentError = abs(lineSentRate-lineHumanRate)
+print(sentError)
