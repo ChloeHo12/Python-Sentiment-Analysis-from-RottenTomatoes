3770b5ea3385cb67b733a86a296df2783522cd43
Chloe Ho <98048503+chloeho12@users.noreply.github.com>
2022-04-18 19:42:25 +0000

    B3e - compute average error

--- main.py
+++ main.py
@@ -3,38 +3,45 @@
 
 from functions import *
 
-#a
-valid = open('validation.txt')
-line = valid.readline()
+# #a
+# valid = open('validation.txt')
+# line = valid.readline()
 # print(getSentRate(line))
 # print(getRevText(line))
 
 # #b
 # rev = getRevText(line)
 # #Computer's rating
-# print(textSent(rev))
+# print("Computer Sentiment Rating:", textSent(rev))
 # #Actual human rating: 1 (negative)
-# print(getSentRate(line))
+# print("Human Sentiment Rating:", getSentRate(line))
 
 # #c
 # lineSentRate = textSent(line)
 # lineHumanRate = getSentRate(line)
-# sentError = abs(lineSentRate-lineHumanRate)
-# print(sentError)
+# sentError = abs(lineSentRate - lineHumanRate)
+# print("The error between computed rating and human rating:", sentError)
 
-#d
+#d + e
+valid = open('validation.txt')
+line = valid.readline()
 count = 0
 lineSentRate = 0
 lineHumanRate = 0
+errorTot = 0
 while line:
 	count += 1
 	lineSentRate = textSent(line)
 	lineHumanRate = getSentRate(line)
 	sentError = abs(lineSentRate-lineHumanRate)
+	errorTot += sentError
 	print("Review", count)
 	print("Computer Sentiment Rating:", lineSentRate)
 	print("Human Sentiment Rating:", lineHumanRate)
 	print("The error between computed rating and human rating:", sentError)
 	print("\n")
 	line = valid.readline()
+
+errorAvg = errorTot/count
+print("The error average:", errorAvg)
 
