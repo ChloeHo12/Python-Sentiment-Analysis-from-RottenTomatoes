5df4728b287c137c4b65fb9a292f5a94023b9674
Chloe Ho <98048503+chloeho12@users.noreply.github.com>
2022-04-18 19:34:05 +0000

    B3b - use textSent()

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ line = valid.readline()
 print(getSentRate(line))
 print(getRevText(line))
 
+rev = getRevText(line)
+#Computer's rating
+print(textSent(rev))
+#Actual human rating: 1 (negative)
+print(getSentRate(line))
